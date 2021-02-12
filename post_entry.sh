d6dd6d654fa1aa2c55c9a920ccb4b4832f1d4e78
Rasukarusan <gta_youtube@yahoo.co.jp>
2021-02-12 10:14:46 +0900

    fix indent

--- post_entry.sh
+++ post_entry.sh
@@ -11,20 +11,20 @@ API_KEY=$(cat ~/account.json | jq -r '.hatena.api_key')
 entry() {
   local title=$1
   local content=$2
-  cat <<EOS
-    <?xml version="1.0" encoding="utf-8"?>
-    <entry xmlns="http://www.w3.org/2005/Atom"
-           xmlns:app="http://www.w3.org/2007/app">
-      <title>${title}</title>
-      <content type="text/plain">
-      ${content}
-      </content>
-      <updated></updated>
-      <category term="" />
-      <app:control>
-        <app:draft>yes</app:draft>
-      </app:control>
-    </entry>
+cat <<EOS
+<?xml version="1.0" encoding="utf-8"?>
+<entry xmlns="http://www.w3.org/2005/Atom"
+       xmlns:app="http://www.w3.org/2007/app">
+  <title>${title}</title>
+  <content type="text/plain">
+${content}
+  </content>
+  <updated></updated>
+  <category term="" />
+  <app:control>
+    <app:draft>yes</app:draft>
+  </app:control>
+</entry>
 EOS
 }
 
@@ -33,4 +33,4 @@ main() {
   local content=$2
   entry "$title" "$content" | curl -s -u ${ID}:${API_KEY} -X POST https://blog.hatena.ne.jp/${ID}/${BLOG_ID}/atom/entry --data-binary @-
 }
-main
+main "$@"
